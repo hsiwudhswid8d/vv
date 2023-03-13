5490accd5ba5a0c470d6f802e2530cf0694f97ec
hsiwudhswid8d <127153178+hsiwudhswid8d@users.noreply.github.com>
2023-03-13 23:27:54 +0300

    Create __init__.py

---
+++ YukkiMusic/utils/database/__init__.py
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/hsiwudhswid8d >.
+#
+# This file is part of < https://github.com/hsiwudhswid8d/vv > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/hsiwudhswid8d/vv/LICENSE >
+#
+# All rights reserved.
+
+from .assistantdatabase import *
+from .memorydatabase import *
+from .mongodatabase import *
